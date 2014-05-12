83fa19eb0fcf12e34b2911d919351cd9bfc58d1c
Thomas Frow <tommy.frow@gmail.com>
2014-05-12 17:52:55 +0100

    more work on navbar

--- js/cookie.js
+++ js/cookie.js
@@ -1,15 +1,11 @@
 
 
-function writeCookie(){
-	alert("writing cookie");
-	document.cookie ="hasAccessedBefore=Yes";
-}
-
 $('#cookieAgreed').click(function(){ 
-	document.cookie ="hasAccessedBefore=Yes";
 	alert("clicked");
 	}
 );
+
+
 
 
 
