b211f97bf1dff0ea686a76adc52c362834992f43
Thomas Frow <tommy.frow@gmail.com>
2014-06-06 18:46:51 +0100

    finished v1

--- js/javascript.js
+++ js/javascript.js
@@ -1,13 +1,26 @@
 
 
 $(document).ready(function() {
-    
-    $('.carousel').each(function(){
+
+  var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
+
+  if (w > 700) {
+
+      $('.carousel').each(function(){
     $(this).carousel({
         pause: 'hover',
             interval: 2500
         });
      });
+
+  } else{
+
+    $('.carousel').each(function(){
+    $(this).carousel("pause");
+     });
+  };
+    
+
 });
 
 
